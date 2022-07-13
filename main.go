d5f42f8e50d22bffe013c23bab86af186da1c182
w.sidonio <w.sidonio@ecotrace.info>
2022-07-12 23:12:49 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your tickets here to attend")
+
+	var conferenceName = "Go Conference"
+	fmt.Println(conferenceName)
+}
